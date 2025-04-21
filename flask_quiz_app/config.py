97f9ec559f04b47cc972ec7337b4de59abfd2f61
goksey5 <goksey5@gmail.com>
2025-04-21 10:08:18 +0300

    config path

--- flask_quiz_app/config.py
+++ flask_quiz_app/config.py
@@ -5,7 +5,8 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "fallback-secret-key")
-    SQLALCHEMY_DATABASE_URI = "sqlite:///D:/python_quiz_app/instance/quiz.db"
+    # Veritabanı yolunu PythonAnywhere ortamına uygun hale getirin
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///instance/quiz.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
